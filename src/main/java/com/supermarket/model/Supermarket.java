1dc4eeba5c5ecbd21ae30eb1954faaa038131124
Florian Iancu <florianiancu@ymail.com>
2025-08-29 17:58:57 +0300

    Checkout functionality

--- src/main/java/com/supermarket/model/Supermarket.java
+++ src/main/java/com/supermarket/model/Supermarket.java
@@ -3,26 +3,37 @@ package com.supermarket.model;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Supermarket {
 
-    private final List<Item> items;
+    private final Map<String, Item> items;
 
     public Supermarket(String pricingTable) {
         this.items = this.parsePricingTable(pricingTable);
     }
 
-    public List<Item> getItems() {
-        return List.copyOf(items);
+    public Double checkout(List<String> scannedItems){
+        Map<String, Long> itemsAndQuantitiesInBasket = scannedItems.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return itemsAndQuantitiesInBasket.entrySet().stream().map(this::getPrice).reduce(0.0, Double::sum);
     }
 
-    private List<Item> parsePricingTable(String pricingTable) {
+    public List<Item> getItems() {
+        return List.copyOf(items.values());
+    }
+
+    private Map<String, Item> parsePricingTable(String pricingTable) {
         try {
             String[] splitLines = pricingTable.split("\n");
             if (isTableHeaderPresent(splitLines)) {
-                return Arrays.stream(splitLines).skip(2).map(this::parsePricingTableLine).toList();
+                return Arrays.stream(splitLines).skip(2)
+                        .map(this::parsePricingTableLine)
+                        .collect(Collectors.toUnmodifiableMap(item -> item.name().toLowerCase(), Function.identity()));
             }
-            throw new RuntimeException("Table header not present.");
+            throw new RuntimeException("Table header does not match format.");
         } catch (Exception e) {
             throw new RuntimeException("Error parsing pricing table.", e);
         }
@@ -44,6 +55,9 @@ public class Supermarket {
 
     private Item parsePricingTableLine(String line) {
         String[] lineElements = line.substring(1).split("[|]");
+        if(lineElements.length != 3) {
+            throw new RuntimeException("The number of columns do not match the format.");
+        }
         return new Item(lineElements[0].trim(), Double.parseDouble(lineElements[1]),
                 lineElements[2].trim().equals("-") ? null : this.buildOffer(lineElements[2]));
     }
@@ -51,6 +65,14 @@ public class Supermarket {
     private Offer buildOffer(String offerText) {
         String[] offerElements = offerText.trim().split(" ");
         return new Offer(Double.parseDouble(offerElements[2]), Integer.parseInt(offerElements[0]));
+    }
+
+    private Double getPrice(Map.Entry<String, Long> itemAndQuantity) {
+        Item item = this.items.get(itemAndQuantity.getKey().trim().toLowerCase());
+        return (item.offer() == null) ?
+                itemAndQuantity.getValue() * item.price()
+                : (itemAndQuantity.getValue() / item.offer().quantity()) * item.offer().price() +
+                itemAndQuantity.getValue() % item.offer().quantity() * item.price();
     }
 
 }
--- src/test/java/com/supermarket/model/SupermarketTest.java
+++ src/test/java/com/supermarket/model/SupermarketTest.java
@@ -2,6 +2,8 @@ package com.supermarket.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -11,7 +13,7 @@ class SupermarketTest {
     void supermarket_whenTableIsEmpty() {
         Supermarket supermarket = new Supermarket("""
                 | Item |Price for 1 item | Offer |
-                |--------|-----------------|---------------------|""");
+                |------|-----------------|-------|""");
 
         assertTrue(supermarket.getItems().isEmpty());
     }
@@ -21,11 +23,11 @@ class SupermarketTest {
         Supermarket supermarket = new Supermarket("""
                 | Item | Price for 1 item | Offer |
                 |--------|-----------------|---------------------|
-                | Apple | 10 | - |""");
+                | Apple | 30 | - |""");
 
         assertEquals(1, supermarket.getItems().size());
         assertEquals("Apple", supermarket.getItems().getFirst().name());
-        assertEquals(10, supermarket.getItems().getFirst().price());
+        assertEquals(30, supermarket.getItems().getFirst().price());
         assertNull(supermarket.getItems().getFirst().offer());
     }
 
@@ -34,11 +36,24 @@ class SupermarketTest {
         Supermarket supermarket = new Supermarket("""
                 |Item|Price for 1 item|Offer|
                 |-|-|-|
-                |Apple|10|-|""");
+                |Apple|30|-|""");
 
         assertEquals(1, supermarket.getItems().size());
         assertEquals("Apple", supermarket.getItems().getFirst().name());
-        assertEquals(10, supermarket.getItems().getFirst().price());
+        assertEquals(30, supermarket.getItems().getFirst().price());
+        assertNull(supermarket.getItems().getFirst().offer());
+    }
+
+    @Test
+    void supermarket_whenMultipleSpaces() {
+        Supermarket supermarket = new Supermarket("""
+                | Item   | Price for 1 item | Offer     |
+                |--------|------------------|-----------|
+                | Apple  | 30               | -         |""");
+
+        assertEquals(1, supermarket.getItems().size());
+        assertEquals("Apple", supermarket.getItems().getFirst().name());
+        assertEquals(30, supermarket.getItems().getFirst().price());
         assertNull(supermarket.getItems().getFirst().offer());
     }
 
@@ -75,6 +90,31 @@ class SupermarketTest {
     }
 
     @Test
+    void supermarket_whenTableHasWrongFormat() {
+        assertThrows(RuntimeException.class, () -> new Supermarket("""
+                | Item   | Weight | Price | Offer     |
+                |--------|--------|-------|-----------|
+                | Apple  | 10     | 10    | 2 for 45  |"""));
+    }
+
+    @Test
+    void supermarket_whenTableLineHasWrongFormat() {
+        assertThrows(RuntimeException.class, () -> new Supermarket("""
+                | Item   | Price | Offer     |
+                |--------|-------|-----------|
+                | Apple  | 10    | 10    | 2 for 45  |"""));
+    }
+
+    @Test
+    void supermarket_whenDuplicateItems() {
+        assertThrows(RuntimeException.class, () -> new Supermarket("""
+                | Item   | Price for 1 item | Offer     |
+                |--------|------------------|-----------|
+                | Apple  | 30               | 2 for 45  |
+                | apple  | 30               | -         |"""));
+    }
+
+    @Test
     void supermarket_whenPriceCanNotBeParsed() {
         assertThrows(RuntimeException.class, () -> new Supermarket("""
                 | Item | Price for 1 item | Offer |
@@ -92,4 +132,67 @@ class SupermarketTest {
         );
     }
 
+    @Test
+    void checkout_whenOneItemGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(30, supermarket.checkout(List.of("Apple")));
+    }
+
+    @Test
+    void checkout_whenIdenticalItemsGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(120, supermarket.checkout(List.of("Peach", "Peach")));
+    }
+
+    @Test
+    void checkout_whenDifferentItemsGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(110, supermarket.checkout(List.of("Apple", "Peach", "Kiwi")));
+    }
+
+    @Test
+    void checkout_whenItemsWithOfferGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(45, supermarket.checkout(List.of("Apple", "Apple")));
+    }
+
+    @Test
+    void checkout_whenMoreItemsThanOfferGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(75, supermarket.checkout(List.of("Apple", "Apple", "Apple")));
+    }
+
+    @Test
+    void checkout_whenMorFewerItemsThanOfferGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(100, supermarket.checkout(List.of("Banana", "Banana")));
+    }
+
+    @Test
+    void checkout_whenItemsWithOfferGiven2(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(90, supermarket.checkout(List.of("Apple", "Apple", "Apple", "Apple")));
+    }
+
+    @Test
+    void checkout_whenItemsWithOfferInRandomOrderGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(65, supermarket.checkout(List.of("Apple", "Kiwi", "Apple")));
+    }
+
+    @Test
+    void checkout_whenItemsWithNonStandardNamesGiven(){
+        Supermarket supermarket = this.buildSupermarket();
+        assertEquals(160, supermarket.checkout(List.of("apple", "BANANA", "  kiwi   ", " pEaCh ")));
+    }
+
+    private Supermarket buildSupermarket(){
+        return new Supermarket("""
+                | Item   | Price for 1 item | Offer     |
+                |--------|------------------|-----------|
+                | Apple  | 30               | 2 for 45  |
+                | Banana | 50               | 3 for 130 |
+                | Peach  | 60               | -         |
+                | Kiwi   | 20               | -         |""");
+    }
 }
